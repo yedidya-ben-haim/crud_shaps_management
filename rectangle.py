93707c3b7b7839846aed30fe9d7256111b6dfd6b
yedidya ben haim <bhyedidya@gmail.com>
2026-05-27 10:43:37 +0300

    Adding documentation and type hinting to rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,18 +1,33 @@
 from shape import Shape
-
+    """
+    Represents a rectangle shape, inheriting from the base Shape class.
+    """
 class Rectangle(Shape):
-    def __init__(self, shape_id, shape_type, width, height):
+
+    def __init__(self, shape_id, shape_type, width, height) -> None:
+        """
+            Initializes a new Rectangle instance.
+        """
         super().__init__(shape_id, shape_type)
         self.width = width
         self.height = height
 
-    def get_area(self):
+    def get_area(self) -> float:
+        """
+            Calculates the area of the rectangle (width * height).
+        """
         return self.width * self.height
 
-    def get_perimeter(self):
+    def get_perimeter(self) -> float:
+        """
+            Calculates the perimeter of the rectangle (2 * (width + height)).
+        """
         return (self.width + self.height)*2
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
+        """
+            Converts the rectangle properties into a dictionary format.
+        """
         return {"id":self.id,
                 "type":self.shape_type,
                 "width":self.width,
