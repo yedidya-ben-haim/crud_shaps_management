de3137da518e7f71f224b2db8fdb4342c422923c
yedidya ben haim <bhyedidya@gmail.com>
2026-05-27 10:14:51 +0300

    Adding documentation and type hinting to square.py

--- square.py
+++ square.py
@@ -1,17 +1,34 @@
 from shape import Shape
 
 class Square(Shape):
-    def __init__(self, shape_id, shape_type, side):
-        super().__init__(shape_id, shape_type)
+    """
+        Represents a square shape, inheriting from the base Shape class.
+    """
+    def __init__(self, shape_id, side):
+        """
+            Initializes a new Square instance.
+        """
+        super().__init__(shape_id, "square")
         self.side = side
 
+
     def get_area(self):
+        """
+            Calculates the area of the square.
+        """
         return self.side * self.side
 
+
     def get_perimeter(self):
+        """
+            Calculates the perimeter of the square.
+        """
         return self.side * 4
 
     def to_dict(self):
+        """
+            Converts the square properties into a dictionary format.
+        """
         return {"id":self.id,
                 "type":self.shape_type,
                 "side":self.side}
