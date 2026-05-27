579e5abb941a8bfca7b3d71b733e8297fb6ead29
yedidya ben haim <bhyedidya@gmail.com>
2026-05-27 10:07:54 +0300

    Adding documentation and type hinting to Shape.py

--- shape.py
+++ shape.py
@@ -1,17 +1,31 @@
 class Shape:
+    """
+        A base class to represent a generic geometric shape
+    """
 
-    def __init__(self, shape_id, shape_type):
+    def __init__(self, shape_id: int, shape_type: str):
         """
-            init function
+            Initializes a new Shape instance
         """
         self.id = shape_id
         self.shape_type = shape_type
 
-    def get_area(self):
+    def get_area(self) -> float:
+        """
+            Calculates and returns the area of the shape.
+            This method should be overridden by subclasses.
+        """
         pass
 
-    def get_perimeter(self):
+    def get_perimeter(self) -> float:
+        """
+            Calculates and returns the perimeter of the shape.
+            This method should be overridden by subclasses
+        """
         pass
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
+        """
+            Converts the shape attributes into a dictionary for JSON file.
+        """
         pass
