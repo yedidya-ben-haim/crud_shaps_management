cc34e820bd8f56d810f3fbbfd33b04b518347d75
yedidya ben haim <bhyedidya@gmail.com>
2026-05-27 11:36:58 +0300

    Updating a .gitignore file

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from shape_manager import ShapeManager
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
     handlers=[
-        logging.FileHandler("app.log", encoding='utf-8'),
-        logging.StreamHandler()
+        logging.FileHandler("app.log", encoding='utf-8')
     ]
 )
 
@@ -16,9 +15,12 @@ logger = logging.getLogger(__name__)
 def main():
     logger.info("The program started running.")  # רישום הודעת מידע
     manager = ShapeManager()
+    sqr = Square(1, 5)
+    print(sqr.to_dict())
 
     # ... כאן יבוא שאר הקוד של התפריט שלך ...
 
 
 if __name__ == "__main__":
     main()
+
